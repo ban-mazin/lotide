35f23334baf31d2f7e974b22a37d1fa736632530
ban-mazin <ban.qaddoori89@gmail.com>
2021-03-07 02:17:11 +0000

    rearrange the folder

--- lotide/head.js
+++
@@ -1,9 +0,0 @@
-const head = function(array) {
-  return array[0];
-};
-
-module.exports = head;
-
-  // TEST CODE
-  assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- lotide/tail.js
+++
@@ -1,7 +0,0 @@
-const tail = function(array) {
-  return array.slice(1);
-};
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
